5321cc62ff09b62c2139b43e5190a3597fd987d6
Enigmaticpravin <pravingupta2020@gmail.com>
2023-08-10 12:40:26 +0530

    Initial commit

---
+++ app/src/main/java/com/opion/knowlio/MainActivity.java
@@ -0,0 +1,28 @@
+package com.opion.knowlio;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Handler;
+import android.view.View;
+
+
+import com.opion.knowlio.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+
+
+}
---
+++ app/src/main/java/com/opion/knowlio/SplashActivity.java
@@ -0,0 +1,69 @@
+package com.opion.knowlio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.opion.knowlio.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        Animation slideUpAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_up);
+        slideUpAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                binding.logo.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                // Animation has ended, you can transition to the next screen here
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Start the main activity here
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        if (user!=null){
+                            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                            Pair[] pairs = new Pair[1];
+                            pairs[0] = new Pair<View, String>(binding.logo, "imageTransition");
+                            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SplashActivity.this, pairs);
+                            startActivity(intent, options.toBundle());
+                            finish(); // Close the splash activity
+                        }
+                    }
+                }, 1500);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        binding.logo.startAnimation(slideUpAnimation);
+    }
+
+}
+}
